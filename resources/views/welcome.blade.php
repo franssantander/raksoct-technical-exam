bc4b95c17faace0b8840d740bf68096cdadd39a9
francesantander <santanderfrancisbeam@gmail.com>
2025-05-22 09:25:23 +0800

    update env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1588,10 +1588,10 @@
                 </ul>
                 <ul class="flex gap-3 text-sm leading-normal">
                     <li>
-                       <a href="mailto:santanderfrancisbeam@gmail.com" 
-                        class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                        <a href="mailto:santanderfrancisbeam@gmail.com"
+                            class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                             Email me
-                         </a>
+                        </a>
                     </li>
                 </ul>
             </div>
